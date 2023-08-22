4267d93597ebd296e0c2d1cb0114f778ed2e8136
Luis Villegas <luiis.mediina9456@gmail.com>
2023-08-22 19:37:41 -0400

    Cambios en el HTML Y MAIN

---
+++ main.js
@@ -0,0 +1,32 @@
+import monedas from './app.js'
+import grafico from './grafico.js'
+grafico('dolar');
+monedas();
+
+let elemento = document.getElementById("combobox");
+let valorElemento = elemento.options[elemento.selectedIndex].value;
+let textoElemento = elemento.options[elemento.selectedIndex].text;
+
+function seleccionaIndicador() {
+    elemento = document.getElementById("combobox");
+    valorElemento = elemento.options[elemento.selectedIndex].value;
+    textoElemento = elemento.options[elemento.selectedIndex].text;
+
+    const nuevograph = document.getElementById("graphbox");
+    nuevograph.setAttribute("id", "graphbox")
+    nuevograph.innerHTML = ``
+    nuevograph.innerHTML = `
+    <div class="chart-container">
+        <canvas id="mychart" ></canvas>
+    </div>`
+    const titulo = document.getElementById("graphtitle")
+    titulo.innerHTML = `
+    <h2>${textoElemento}</h2>
+    `
+
+    grafico(valorElemento);
+}
+
+document
+    .getElementById("combobox")
+    .addEventListener("change", seleccionaIndicador);
